bf3b287876c1c616f91f1afc1a11c3ae19e10373
Ejiroghene Obiuwevbi <ejiroghene15@gmail.com>
2020-08-20 22:55:43 +0100

    deleted unsed model

--- src/model/kitty.js
+++
@@ -1,6 +0,0 @@
-import mongoose, { kittySchema } from "./db";
-
-const Kitten = mongoose.model("Kitten", kittySchema);
-
-export const silence = new Kitten({ name: "Silence" });
-console.log(silence.name); // 'Silence'
